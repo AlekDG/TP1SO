11b4712a1989ba4fad2772be785eb967f526803e
Blemmingan <95772227+Blemmingan@users.noreply.github.com>
2024-04-16 09:35:27 -0300

    git broke again, manual commit

--- utils.h
+++ utils.h
@@ -16,6 +16,55 @@
 #define ID_SIZE 256
 #define MEM_SIZE 1024
 
+#ifndef UTILS_H
+#define UTILS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+
+#define CHILD 0
+#define ERROR -1
+#define STDIN 0
+#define PIPE_FD_ARR_SIZE 2
+#define READ_END 0
+#define WRITE_END 1
+
+#include <stdlib.h>
+
+#define END_TOKEN '$'
+#define END_STR "$"
+
+// Data structure provided for the user to hold all the information needed about this shared memory.
+typedef struct shared_memory_CDT *shared_memory_ADT;
+
+//  Creates the shared memory, using the size required as a parameter.
+shared_memory_ADT initialize_shared_memory(size_t shm_size);
+
+//  Opens the shared memory previously created, it will fail if we try to open without having one created.
+shared_memory_ADT open_shared_memory(size_t shm_size);
+
+// It writes the given string to the shared memory.
+void write_to_shared_memory(shared_memory_ADT shm, char* buffer, size_t size);
+
+/*
+**  It reads the shared memory and writes the result on the buffer, it returns the number of
+**  characters read, or EOF if we reached the end of file.
+*/
+size_t read_shared_memory(shared_memory_ADT shm, char *buffer, size_t size);
+
+// Closes, but not removes the shared memory object from the process.
+void close_shared_memory(shared_memory_ADT shm);
+
+// Closes and removes the shared memory of all processes, should be called after all the processes closed the shm before.
+void unlink_shared_memory_resources(shared_memory_ADT shm);
+
+void exitOnError(char* msg);
+
+
+#endif
+
 void exitOnError(char* msg);
 typedef struct memoryStruct{
     char fileID[ID_SIZE];
