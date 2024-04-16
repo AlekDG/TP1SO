16b9c523fbc109c684b6d0f6ede5dc3c1f97e8e7
Alejo P.M <66072172+AlekDG@users.noreply.github.com>
2024-04-15 23:31:46 -0300

    Added Header for MSH API

--- utils.h
+++ utils.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <bits/semaphore.h>
 
 
 #define CHILD 0
@@ -12,8 +13,26 @@
 #define PIPE_FD_ARR_SIZE 2
 #define READ_END 0
 #define WRITE_END 1
+#define ID_SIZE 256
+#define MEM_SIZE 1024
 
 void exitOnError(char* msg);
+typedef struct memoryStruct{
+    char fileID[ID_SIZE];
+    int flag;
+    sem_t sem;
+    char map[MEM_SIZE];
+} memStruct;
+typedef memStruct* memADT;
 
+void exitOnError(char* msg);
+memADT createSharedMemory(void);
+memADT openExistingMemory(char*id);
+void unlinkMemory(memADT m);
+void setFlag(memADT m,int val);
+int getFlag(memADT m);
+sem_t* getMemorySem(memADT m);
+char* getMemoryMap(memADT m);
+char* getMemoryID(memADT m);
 
 #endif
