1b811d0758e27accee8fd4d644dd9d183abaacf3
Alejo P.M <66072172+AlekDG@users.noreply.github.com>
2024-04-17 04:00:54 -0300

    Implemented bash pipe functionality

--- view.c
+++ view.c
@@ -13,30 +13,32 @@ int connectToApp(char* buffer);
     sem_t* full ;
 
 int main(int argc, char* argv[]){
-    char* appOutput = malloc(ID_SIZE);
+    char* appOutput=NULL;
+    size_t length=0;
     if(argc==2){
         appOutput = argv[1];
         printf("arg %s\n",appOutput);
     } else {
-        read(0,appOutput,ID_SIZE);
-        
+        ssize_t len = getline(&appOutput,&length,stdin);
+        if(appOutput[len-1]=='\n')
+            appOutput[len-1]='\0';
+        printf("%s",appOutput);
     }
 
     memADT sharedMem = openExistingMemory(appOutput);
     critcalRegion = sem_open(CRITICAL_REGION_SEM,O_RDWR);
-  empty = sem_open(EMPTY_SEM,O_RDWR);
-  full = sem_open(FULL_SEM,O_RDWR);
+    empty = sem_open(EMPTY_SEM,O_RDWR);
+    full = sem_open(FULL_SEM,O_RDWR);
     puts("PASO shm");
     if(sharedMem == NULL){
         puts("PASO null");
         exitOnError("Failed to open memory");
     }
     writeOutput(sharedMem);
-    unlinkMemory(sharedMem);
-        sem_close(critcalRegion);
+    free(sharedMem);
+    sem_close(critcalRegion);
     sem_close(empty);
     sem_close(full);
-    free(appOutput);
     exit(0);
 }
 
@@ -48,25 +50,18 @@ void writeOutput(memADT mem){
 
     while(st){
      
-     int val;
-     sem_getvalue(full,&val);
+        int val;
+        sem_getvalue(full,&val);
         printf("full %d\n",val);
-
-
         sem_wait(full);
-
-
-         sem_getvalue(critcalRegion,&val);
+        sem_getvalue(critcalRegion,&val);
         printf("cr %d\n",val);
-
         
         sem_wait(critcalRegion);
-        //strcpy(buffer,mapPtr);
         int i = 0;
         for(; mapPtr[i]!='\n' && mapPtr[i]!='\0';i++){
             buffer[i] = mapPtr[i];
         }
-        //buffer[i++] = '\n';
         buffer[i] = '\0';
         if(*mapPtr != STOP) mapPtr++;
         if(buffer[0] != STOP){
@@ -78,6 +73,5 @@ void writeOutput(memADT mem){
         }
         sem_post(empty);
         mapPtr+=strlen(buffer);
-
     }
 }
